1c45b871194e3b7916dc531a2454dcbb23c3d0d3
JunhongOu <junhongo@nycstudents.net>
2024-12-20 03:43:56 +0000

    second commit

--- src/main/java/com/example/project/Day2.java
+++ src/main/java/com/example/project/Day2.java
@@ -1,13 +1,30 @@
-
 package com.example.project;
 import java.util.Random;
 
 public class Day2{
-    public static String[][] nameSort(String[] names){ // your will be tested on this method
-        return null; //you must return a two dimensional string array
-    }
+    public static String[][] nameSort(String[] names){ 
+      Random random = new Random();
+      String[] niceList = new String[names.length];
+      String[] naughtyList = new String[names.length];
+      int niceLen = 0;
+      int naughtyLen = 0;
+      
+        for(int i = 0; i < names.length; i++){
+            
+            int assign = random.nextInt(2);
+            switch (assign){
+                case 0:
+                    niceList[niceLen] = names[i];
+                    niceLen++;
+                    break;
+                case 1:
+                    naughtyList[naughtyLen] = names[i];
+                    naughtyLen++;
+                    break;
+            }
+        }
 
-    public static void main(String[] args) {
-
+        String[][] fullList = {niceList, naughtyList};
+        return fullList; 
     }
 }
--- src/main/java/com/example/project/Day3.java
+++ src/main/java/com/example/project/Day3.java
@@ -1,20 +1,39 @@
 package com.example.project;
 
-public class Day3 {
-      public static String[][] generateSnowflake(int size) { //you will be tested on this method
-        String[][] grid = new String[size][size];
+public class Day3 
+{
+      public static String[][] generateSnowflake(int size){ 
+        //you will be tested on this method
+        String[][] arrayGrid = new String[size][size];
 
-    
+        for (int i = 0; i < size; i++){
+          arrayGrid[i][size / 2] = "*"; 
+          arrayGrid[size / 2][i] = "*"; 
+          arrayGrid[i][i] = "*";      
+          arrayGrid[i][size - 1 - i] = "*"; 
+        }
 
-        return grid;
-    }
+        for (int z = 0; z < size; z++){
+          for (int j = 0; j < size; j++){
+            arrayGrid[z][j] = (arrayGrid[z][j] == null) ? " " : arrayGrid[z][j];
+          }
+        }
+
+        return arrayGrid;
+      }
 
     // Prints the snowflake  will be useful if tests fail (you will not be tested on this method)
-    public static void printSnowflake(String[][] snowflake) {
-        
+    public static void printSnowflake(String[][] snowflake){
+      for (int z = 0; z < snowflake.length; z++){
+        for (int j = 0; j < snowflake[z].length; j++){
+          System.out.print(snowflake[z][j]);
+      }
+      System.out.println("");
+      }
     }
 
     // Test for the snowflake generation
     public static void main(String[] args) {
+      printSnowflake(generateSnowflake(4));
     }
 }
